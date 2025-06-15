4a121ba1ac9ffff18a6a590c5bac4d39ec3d4282
User <hj.mezal@uea.edu.ec>
2025-06-15 13:33:57 -0500

    primer commit

---
+++ src/main/java/com/mycompany/tecnicasdeprogramacionuea2/TecnicasDeProgramacionUEA2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tecnicasdeprogramacionuea2;
+
+/**
+ *
+ * @author User
+ */
+public class TecnicasDeProgramacionUEA2 {
+
+    public static void main(String[] args) {
+        System.out.println(" Hola mundo!");
+    }
+}
